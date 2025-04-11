7aa9885541f4019ec5fb84baafdb3468e07d8f7d
stepanleas <s.leas@developmentaid.org>
2025-04-11 09:53:17 +0300

    restructure application using modules

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::net::TcpListener;
-use zero_to_production_in_rust::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind random port");
 
-    run(listener)?.await
+    zero_to_production_in_rust::startup::run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,7 +3,7 @@ use std::net::TcpListener;
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero_to_production_in_rust::run(listener).expect("Failed to bind address");
+    let server = zero_to_production_in_rust::startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)
