ce509613613c7c364222945260e90e16038ff628
stepanleas <s.leas@developmentaid.org>
2025-04-16 11:08:23 +0300

    update application to use db pool and persist subscriptions in table

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::{Connection, PgPool};
 use std::net::TcpListener;
 use zero_to_production_in_rust::configuration::get_configuration;
 use zero_to_production_in_rust::startup::run;
@@ -6,7 +7,10 @@ use zero_to_production_in_rust::startup::run;
 async fn main() -> Result<(), std::io::Error> {
     let config = get_configuration().expect("Failed to read configuration");
     let address = format!("127.0.0.1:{}", config.application_port);
-
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    let connection_pool = PgPool::connect(&config.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    run(listener, connection_pool)?.await
 }
