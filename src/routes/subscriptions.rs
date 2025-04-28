47b2a1492777863d44d896b4cd345ba896092a07
stepanleas <s.leas@developmentaid.org>
2025-04-28 19:31:09 +0300

    implement tracing for subscriptions endpoint

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,6 +13,18 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     db_pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name,
+    );
+
+    let _request_span_guard = request_span.enter();
+
+    tracing::info!("request_id {} - Saving new subscriber details in the database", request_id);
+
     match sqlx::query!(
         r#" INSERT INTO subscriptions (id, email, name, subscribed_at)  VALUES ($1, $2, $3, $4) "#,
         Uuid::new_v4(),
@@ -23,9 +35,12 @@ pub async fn subscribe(
     .execute(db_pool.as_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!("request_id {} - New subscriber details have been saved", request_id);
+            HttpResponse::Ok().finish()
+        },
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
